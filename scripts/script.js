bf22776c40683176a67e5be65964c9c5132fccbf
Jubakee <jubakee@gamil.com>
2024-07-18 20:07:25 +1200

    test

--- scripts/script.js
+++ scripts/script.js
@@ -1,53 +1,63 @@
 Telegram.WebApp.ready();
 Telegram.WebApp.expand();
-Telegram.WebApp.disableSwipe();
-Telegram.WebApp.disableSwipeBack();
 document.addEventListener('DOMContentLoaded', () => {
 
 })
 
 window.addEventListener('load', () => {
 loadCounter();
+setupTabEventListeners();
+Telegram.WebApp.disableSwipeBack();
 });
 
-
+//#region Load & Save
 function loadCounter() {
     const savedCoins = localStorage.getItem('avatar_coins');
     const savedEnergy = localStorage.getItem('avatar_energy');
     const savedLastUpdate = localStorage.getItem('avatar_lastupdate');
 
-    console.log(savedCoins, savedEnergy, savedLastUpdate);
+    console.log('Coins: ' + savedCoins, 'Energy: ' + savedEnergy);
 
-    // Load coins
     if (savedCoins !== null) {
         coins = parseInt(savedCoins, 10);
         document.getElementById('coins').innerText = coins;
     }
-
-    // Load energy and update if needed
-    // if (savedEnergy !== null && savedLastUpdate !== null) {
-    //     energy = parseInt(savedEnergy, 10);
-    //     lastUpdateTime = parseInt(savedLastUpdate, 10);
-
-    //     // Calculate elapsed time since last update
-    //     const elapsedTime = Date.now() - lastUpdateTime;
-
-    //     // Calculate energy regeneration
-    //     if (elapsedTime > 0) {
-    //         const energyToAdd = Math.floor(elapsedTime / rechargeInterval) * energyRechargeRate;
-    //         energy = Math.min(energy + energyToAdd, maxEnergy); // Cap energy at max
-    //     }
-    // }
-
-    // // Update UI with loaded energy
-    // document.getElementById('energy').innerText = energy;
 }
-
 
 function saveCounter() {
     localStorage.setItem('avatar_coins', coins);
     localStorage.setItem('avatar_energy', energy);
     localStorage.setItem('avatar_lastupdate', Date.now());
-    console.log('Saved')
-
+    console.log('Saved to Local Storage.')
 }
+//#endregion
+
+//#region Tabs
+function setupTabEventListeners() {
+    const tabButtons = document.querySelectorAll('.tab');
+
+    tabButtons.forEach(button => {
+        const tabId = button.id.replace('-btn', '');
+        
+        button.addEventListener('click', () => showTab(tabId));
+    });
+}
+
+function showTab(tabId) {
+    const tabs = document.querySelectorAll('main');
+    tabs.forEach(tab => {
+        tab.classList.remove('active');
+    });
+
+    const activeTab = document.getElementById(tabId);
+    activeTab.classList.add('active');
+
+    const buttons = document.querySelectorAll('.tab');
+    buttons.forEach(button => {
+        button.classList.remove('active-tab');
+    });
+    document.getElementById(tabId + '-btn').classList.add('active-tab');
+
+    document.getElementById('coins').innerText = coins;
+}
+//#endregion
