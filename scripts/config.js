cf5031a1abc2915954f26ca592d1c160ca0d4a3a
Jubakee <jubakee@gamil.com>
2024-07-18 18:52:08 +1200

    test swipe 3

---
+++ scripts/config.js
@@ -0,0 +1,7 @@
+let coins = 0;
+let energy = 1000; // Starting energy value
+const maxEnergy = 1000; // Maximum energy value
+const energyRechargeRate = 1; // Energy recharge rate
+const rechargeInterval = 3000; // Recharge every 3 seconds
+
+let lastUpdateTime = Date.now(); // Track last update time
--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,44 @@
 Telegram.WebApp.ready();
 Telegram.WebApp.expand();
-Telegram.WebApp.disableSwipeBack();
 
 document.addEventListener('DOMContentLoaded', () => {
-
+    Telegram.WebApp.disableSwipeBack();
 })
+
+window.addEventListener('load', () => {
+loadCounter();
+});
+
+
+function loadCounter() {
+    const savedCoins = localStorage.getItem('avatar_coins');
+    const savedEnergy = localStorage.getItem('avatar_energy');
+    const savedLastUpdate = localStorage.getItem('avatar_lastupdate');
+
+    console.log(savedCoins, savedEnergy, savedLastUpdate)
+
+
+    // if (savedCoins) {
+    //     coins = parseInt(savedCoins, 10);
+    //     document.getElementById('coins').innerText = coins;
+    // }
+
+    // if (savedEnergy) {
+    //     energy = Math.min(parseInt(savedEnergy, 10), maxEnergy); // Cap energy at max
+    // }
+
+    // if (savedLastUpdate) {
+    //     lastUpdateTime = parseInt(savedLastUpdate, 10);
+    //     const elapsedTime = Date.now() - lastUpdateTime;
+    //     energy += Math.floor(elapsedTime / rechargeInterval) * energyRechargeRate;
+    //     energy = Math.min(energy, maxEnergy); // Cap energy at max
+    // }
+}
+
+function saveCounter() {
+    localStorage.setItem('avatar_coins', coins);
+    localStorage.setItem('avatar_energy', energy);
+    localStorage.setItem('avatar_lastupdate', Date.now());
+    console.log('Saved')
+
+}
