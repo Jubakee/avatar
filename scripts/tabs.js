792f6ddb4e4bdba9e7a286708f9549137c2af248
Jubakee <jubakee@gamil.com>
2024-07-20 20:27:11 +1200

    test changges shop

--- scripts/tabs.js
+++ scripts/tabs.js
@@ -23,8 +23,22 @@ function showTab(tabId) {
     });
     document.getElementById(tabId + '-btn').classList.add('active-tab');
 
-    updateIncomeDisplay();
-    document.getElementById('coins').innerText = coins;
+
+    document.getElementById('coins').innerText = formatNumber(coins);
     
+}
+
+function formatNumber(number) {
+    if (number >= 1e12) {
+        return (number / 1e12).toFixed(2) + 'T';
+    } else if (number >= 1e9) {
+        return (number / 1e9).toFixed(2) + 'B';
+    } else if (number >= 1e6) {
+        return (number / 1e6).toFixed(2) + 'M';
+    } else if (number >= 1e5) {
+        return (number / 1e3).toFixed(0) + 'K';
+    } else {
+        return number.toLocaleString();
+    }
 }
 //#endregion
