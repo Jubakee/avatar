aa13ebdf282e1542b9c528518ecc2b9b422a1c39
Jubakee <jubakee@gamil.com>
2024-07-21 11:05:26 +1200

    inventory 80

--- scripts/init.js
+++ scripts/init.js
@@ -5,7 +5,7 @@ window.Telegram.WebApp.expand();
 window.Telegram.WebApp.disableVerticalSwipes();
 
 window.addEventListener('load', () => {
-    //resetGame();
+    resetGame();
     loadCoins();
 
     loadEnergy();
@@ -15,3 +15,25 @@ window.addEventListener('load', () => {
 
 });
 
+
+//#region Reset Game
+function resetGame() {
+    coins = 0;
+    energy = 1000; // Reset energy to starting value
+    level = 1; // Reset level to starting value
+    coinsPerClick = 100; // Reset coins per click
+
+    // Clear saved data from local storage
+    localStorage.removeItem('avatar_coins');
+    localStorage.removeItem('avatar_energy');
+    localStorage.removeItem('avatar_lastupdate');
+    localStorage.removeItem('avatar_base_income');
+    localStorage.removeItem('avatar_inventory');
+    localStorage.removeItem('last_item_id');
+ 
+    // Update the UI
+    document.getElementById('coins').innerText = coins;
+    updateEnergyBar();
+    updateLevelDisplay();
+}
+//#endregion
